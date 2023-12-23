92c0afaef2b9b8b2efbd8950922ace36b7cb3f2b
nadhemDev <blendplay2@gmail.com>
2023-12-23 18:34:21 +0100

    tailwind setup fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-
+import './global.css';
 const App = () => {
     return (
-        <div> this my App</div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     )
 }
 
